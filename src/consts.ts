b58ef5e166bda4f49ff96bff003cc9d89b30d82c
cinatit <cinatit.igor.santos@gmail.com>
2026-05-30 13:41:59 -0300

    Update og image

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "PoliSat";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "O PoliSat é um grupo da Poli-USP que desenvolve satélites (CubeSats) para fins de competição e pesquisa";
 export const BASE_URL = "/polisat-teste";
